8cc44d7da4cff646d677d23b9cb10d21325b3ed7
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-22 18:28:06 +0530

    Delete resolver-customvalidation directory

--- resolver-customvalidation/src/app/hero/shared/validators/name.validators.ts
+++
@@ -1,7 +0,0 @@
-import { FormControl, ValidationErrors } from '@angular/forms';
-
-export function invalidName(control: FormControl): ValidationErrors | null {
-    const name = control.value;
-    const result = /admin/.test(name)
-    return result ? { invalidName: { value: control.value } } : null
-}
--- resolver-customvalidation/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true
-};
-export const serverAddress = "http://localhost:3000/"
