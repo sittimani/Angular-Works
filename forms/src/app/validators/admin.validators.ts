60beb34c351944d54e1d056d9cfc97938e59f1e8
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-23 18:59:14 +0530

    Delete forms directory

--- forms/src/app/validators/admin.validators.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
-import { Observable, of } from "rxjs";
-
-export function adminName(control: AbstractControl): Observable<ValidationErrors> | Observable<null> {
-    const value = control.value
-    const result = /admin/.test(value)
-    return result ? of({ "admin": {value: value} }) : of(null)
-}
