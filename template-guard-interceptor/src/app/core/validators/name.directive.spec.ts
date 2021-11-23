4a689a11e8f5a51a521d166539c090207dbf6159
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-23 18:59:52 +0530

    Delete template-guard-interceptor directory

--- template-guard-interceptor/src/app/core/validators/name.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NameDirective } from './name.directive';
-
-describe('NameDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NameDirective();
-    expect(directive).toBeTruthy();
-  });
-});
